68aa4bcc2075860066af1c8b3ad0afa8b88449f4
Jonathan Dolan <yonidolan1@gmail.com>
2024-07-01 19:06:20 +0300

    Initial backend setup

---
+++ server.js
@@ -0,0 +1,5 @@
+import express from 'express'
+const app = express()
+app.get('/', (req, res) => res.send('Hello there'))
+const port = 3030
+app.listen(port, () => console.log(`Server ready at port ${port}`))
